124935fe0f98debce62a69b143a5d2b3bb865cb6
yangyan <277160299@qq.com>
2016-02-16 23:25:22 +0800

    增加消息类型的注释说明

--- src/main/java/com/fengyunhe/wechat/mp/MsgType.java
+++ src/main/java/com/fengyunhe/wechat/mp/MsgType.java
@@ -1,5 +1,44 @@
 package com.fengyunhe.wechat.mp;
 
 public enum MsgType {
-    text, image, voice, video,shortvideo, location, link, event, news, music
+    /**
+     * 文本消息类型
+     */
+    text,
+    /**
+     * 图片消息类型
+     */
+    image,
+    /**
+     * 语音消息类型
+     */
+    voice,
+    /**
+     * 视频消息类型
+     */
+    video,
+    /**
+     * 短视频消息类型
+     */
+    shortvideo,
+    /**
+     * 地理位置消息类型
+     */
+    location,
+    /**
+     * 链接消息类型
+     */
+    link,
+    /**
+     * 事件消息类型
+     */
+    event,
+    /**
+     * 图文消息类型
+     */
+    news,
+    /**
+     * 音乐消息类型
+     */
+    music
 }
